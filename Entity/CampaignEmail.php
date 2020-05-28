e9fc6febd8e360de304e891a3f39003957469483
Chadli Ouhichi <louhichi.chadli93@outlook.fr>
2020-05-28 15:22:31 +0100

    bundle capaignEmail Entity

---
+++ Entity/CampaignEmail.php
@@ -0,0 +1,84 @@
+<?php
+
+
+namespace DotIt\SarbacaneBundle\Entity;
+
+
+class CampaignEmail
+{
+    protected $id;
+
+    protected $name;
+
+    protected $kind;
+
+    protected $campaignId;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKind()
+    {
+        return $this->kind;
+    }
+
+    /**
+     * @param mixed $kind
+     */
+    public function setKind($kind)
+    {
+        $this->kind = $kind;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCampaignId()
+    {
+        return $this->campaignId;
+    }
+
+    /**
+     * @param mixed $campaignId
+     */
+    public function setCampaignId($campaignId)
+    {
+        $this->campaignId = $campaignId;
+    }
+
+
+
+
+}
