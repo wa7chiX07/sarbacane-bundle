fc6ba3d12080544bfd573499db27f08f2d305498
Chadli Ouhichi <louhichi.chadli93@outlook.fr>
2020-06-10 10:45:40 +0100

    List Exception handler

--- Services/ListManager.php
+++ Services/ListManager.php
@@ -4,6 +4,7 @@
 namespace DotIt\SarbacaneBundle\Services;
 
 
+use DotIt\SarbacaneBundle\Entity\Log;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
@@ -17,7 +18,15 @@ class ListManager extends BaseManager
     public function getList($limit= null,$offset=null)
     {
         $curl = parent::getCurl(self::$baseUrl.'lists?limit='.$limit.'&offset='.$offset);
-        return json_decode(curl_exec($curl));
+        $result = json_decode(curl_exec($curl));
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if($httpCode>=400)
+        {
+            if($httpCode ==500)
+                $result->message = self::getErrorMessage($result->message);
+            throw new \Exception($result->message);
+        }
+        return $result;
         curl_close($curl);
 
     }
@@ -26,7 +35,25 @@ class ListManager extends BaseManager
         $curl = parent::postCurl(self::$baseUrl.'lists',
             json_encode($name)
         );
-        return json_decode( curl_exec($curl));
+        $result =  json_decode( curl_exec($curl));
+        $httpCode = curl_getinfo($curl,CURLINFO_HTTP_CODE);
+        $log = new Log();
+        $log->setObject('List: creat new list');
+        $log->setStatusCode($httpCode);
+        if($httpCode>=400)
+        {
+            if($httpCode == 500)
+                $result->message = self::getErrorMessage($result->message);
+            $log->setMessage($result->message);
+            self::$container->get('doctrine')->getManager()->persist($log);
+            self::$container->get('doctrine')->getManager()->flush($log);
+            throw new \Exception($result->message);
+        }
+        $log->setIdObject($result->id);
+        $log->setMessage('success');
+        self::$container->get('doctrine')->getManager()->persist($log);
+        self::$container->get('doctrine')->getManager()->flush($log);
+        return $result;
         curl_close($curl);
 
     }
@@ -35,7 +62,25 @@ class ListManager extends BaseManager
         $curl = parent::postCurl(self::$baseUrl.'lists/'.$listId.'/contacts/import',
                 json_encode($contacts)
             );
-        return json_decode(curl_exec($curl));
+        $result = json_decode(curl_exec($curl));
+        $httpCode = curl_getinfo($curl,CURLINFO_HTTP_CODE);
+        $log = new Log();
+        $log->setIdObject($listId);
+        $log->setStatusCode($httpCode);
+        $log->setObject('List: Add contacts to list');
+        if($httpCode>=400)
+        {
+            if($httpCode==500)
+                $result->message = self::getErrorMessage($result->message);
+            $log->setMessage($result->message);
+            self::$container->get('doctrine')->getManager()->persist($log);
+            self::$container->get('doctrine')->getManager()->flush($log);
+            throw new \Exception($result->message);
+        }
+        $log->setMessage('success');
+        self::$container->get('doctrine')->getManager()->persist($log);
+        self::$container->get('doctrine')->getManager()->flush($log);
+        return $result;
         curl_close($curl);
 
     }
